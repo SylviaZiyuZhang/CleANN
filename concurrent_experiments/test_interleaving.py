43f2ec9e42263864027b100b4c58f96bde991255
Josh Engels <josh.adam.engels@gmail.com>
2023-11-20 22:46:54 -0500

    Concurrent experiments written up

--- concurrent_experiments/test_interleaving.py
+++ concurrent_experiments/test_interleaving.py
@@ -1,85 +1,47 @@
-import h5py
 import numpy as np
-import yaml
-import diskannpy
-import time
-import os
-from utils import test_static_index, num_recalled
-
-dynamic_test = diskannpy._diskannpy.run_dynamic_test
+from utils import parse_ann_benchmarks_hdf5, run_dynamic_test, test_static_index
 
 distance_metric = "euclidean"
 dataset_name = "sift-128-euclidean.hdf5"
 dataset_short_name = dataset_name.split("-")[0]
-dimension = int(dataset_name.split("-")[1])
-k = 10
-
-alpha = 1.2
-build_complexity = 64
-graph_degree = 64
-query_complexity = 64
-
-
-def parse_ann_benchmarks_hdf5(data_path):
-    with h5py.File(data_path, "r") as file:
-        gt_neighbors = np.array(file["neighbors"])
-        queries = np.array(file["test"])
-        data = np.array(file["train"])
-
-        return data, queries, gt_neighbors
-
 
 data_path = f"data/{dataset_name}"
 data, queries, gt_neighbors = parse_ann_benchmarks_hdf5(data_path)
 
 data = data[:10000]
+
 num_adversarial_per_data = 16
 adversarial_queries = []
-for i in range(8):
-    for d in data:
+for d in data:
+    for i in range(num_adversarial_per_data):
         adversarial_queries.append(d + 1 * np.random.normal(size=len(d)))
 adversarial_queries = np.array(adversarial_queries)
+queries = adversarial_queries
 
-for strategy in ["Easy", "Hard"]:
-    times = []
-    for num_threads in [1, 2, 4, 8]:
-        start = time.time()
 
-        dynamic_index = diskannpy.DynamicMemoryIndex(
-            distance_metric="l2",
-            vector_dtype=np.float32,
-            dimensions=dimension,
-            max_vectors=len(data),
-            complexity=64,
-            graph_degree=64,
-        )
+# "Easy", all adversarial queries come at the end
+print("Adversarial queries, all at the end:")
 
-        print(data.shape, adversarial_queries.shape)
+indexing_plan = [(0, i) for i in range(len(data))]
+querying_plan = [(1, i) for i in range(len(queries))]
 
-        if strategy == "Easy":
-            updates = [(0, i) for i in range(len(data))] + [
-                (1, i) for i in range(len(adversarial_queries))
-            ]
-        else:
-            updates = [
-                (
-                    i % (num_adversarial_per_data + 1) > 0,
-                    i // (num_adversarial_per_data + 1),
-                )
-                for i in range((num_adversarial_per_data + 1) * len(data))
-            ]
+plans = [
+    ("Indexing", data, [], indexing_plan, None),
+    ("Querying", [], queries, querying_plan, None),
+]
+run_dynamic_test(plans, max_vectors=len(data))
 
-        results = dynamic_test(
-            dynamic_index._index,
-            data,
-            adversarial_queries,
-            updates,
-            query_k=10,
-            query_complexity=64,
-            num_threads=num_threads,
-        )
 
-        end = time.time()
-        times.append(end - start)
+# "Hard", adversarial queries are at the worst position (right after their nearest neighbor)
+print("Adversarial queries, mixed in at the worst position:")
 
-        print(strategy, num_threads, times[-1], times[0] / times[-1])
+indexing_and_querying_plan = []
+for i in range(len(data)):
+    indexing_and_querying_plan.append((0, i))
+    for j in range(num_adversarial_per_data):
+        indexing_and_querying_plan.append((1, i * num_adversarial_per_data + j))
+
+plans = [
+    ("Adversarially Mixed", data, queries, indexing_and_querying_plan, None),
+]
+run_dynamic_test(plans, max_vectors=len(data))
--- concurrent_experiments/test_normal.py
+++ concurrent_experiments/test_normal.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import parse_ann_benchmarks_hdf5, run_dynamic_test
+from utils import parse_ann_benchmarks_hdf5, run_dynamic_test, test_static_index
 
 distance_metric = "euclidean"
 dataset_name = "sift-128-euclidean.hdf5"
@@ -8,17 +8,12 @@ dataset_short_name = dataset_name.split("-")[0]
 data_path = f"data/{dataset_name}"
 data, queries, gt_neighbors = parse_ann_benchmarks_hdf5(data_path)
 
-chunk_size = 8
-for i in range(0, len(data), chunk_size):
-    data[i : i + chunk_size] = data[i] + 0.01 * np.random.normal(
-        size=(chunk_size, len(data[0]))
-    )
-
-data = data[:100000]
-
 indexing_plan = [(0, i) for i in range(len(data))]
+querying_plan = [(1, i) for i in range(len(queries))]
+
 plans = [
     ("Indexing", data, queries, indexing_plan, None),
+    ("Querying", data, queries, querying_plan, None),
 ]
 
 run_dynamic_test(plans, max_vectors=len(data))
