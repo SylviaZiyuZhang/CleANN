f1dd3c19e2cb28388a03e97cf726d9bc3234fe24
SylviaZiyuZhang <sylziyuz@mit.edu>
2024-06-15 22:30:14 -0400

    fix max points for using bitset macro

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ class CMakeExtension(Extension):
         super().__init__(name, sources=[])
         self.sourcedir = os.fspath(Path(sourcedir).resolve())
         self.defined_macros = [
-            ("MAX_POINTS_FOR_USING_BITSET", "10000000"),
             ("EDGE_ANALYTICS_ENABLED", "false"),
             ("PATH_COMPRESSION_ENABLED", "false"),
         ]
