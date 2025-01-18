9085fffebbbcc226a58b948a821574b330e371e5
SylviaZiyuZhang <sylziyuz@csail.mit.edu>
2025-01-18 10:16:48 -0500

    script anonymize

--- concurrent_experiments/batch_run_experiment.sh
+++ concurrent_experiments/batch_run_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets sift-128-euclidean \
     --metrics l2 \
     --sizes 500000 \
@@ -10,8 +10,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > sift_rolling_update_static_recompute.log \
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets glove-100-angular \
     --metrics cosine \
     --sizes 500000 \
@@ -19,8 +19,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > glove_rolling_update_static_recompute.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets adversarial-128-euclidean \
     --metrics l2 \
     --sizes 500000 \
@@ -28,8 +28,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > adversarial_rolling_update_static_recompute.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets huffpost-256-angular \
     --metrics cosine \
     --sizes 50000 \
@@ -37,8 +37,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > huffpost_rolling_update_static_recompute.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets redcaps-512-angular \
     --metrics cosine \
     --sizes 500000\
@@ -46,8 +46,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > redcaps_rolling_update_static_recompute.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets spacev-100-euclidean-30m-samples \
     --metrics l2 \
     --sizes 500000\
@@ -55,8 +55,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > spacev_rolling_update_static_recompute.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets yandextti-200-mips-10m-subset \
     --metrics mips \
     --sizes 500000\
@@ -64,8 +64,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > yandex_rolling_update_static_recompute.log
 
 # python3 experiment_batch_runner.py \
-#     --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-#     --gt_data_prefix "/storage/sylziyuz/" \
+#     --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+#     --gt_data_prefix "/storage/$whoami/" \
 #     --datasets redcaps-512-angular \
 #     --metrics cosine \
 #     --sizes 5000000\
@@ -73,8 +73,8 @@ python3 experiment_batch_runner.py \
 #     --setting_name rolling_update_10m > redcaps_rolling_update_10m.log
 
 python3 experiment_batch_runner.py \
-    --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-    --gt_data_prefix "/storage/sylziyuz/" \
+    --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+    --gt_data_prefix "/storage/$whoami/" \
     --datasets spacev-100-euclidean-30m-samples \
     --metrics l2 \
     --sizes 5000000\
@@ -82,8 +82,8 @@ python3 experiment_batch_runner.py \
     --setting_name rolling_update > spacev_rolling_update_10m.log
 
 # python3 experiment_batch_runner.py \
-#     --data_prefix "/storage/sylziyuz/new_filtered_ann_datasets/" \
-#     --gt_data_prefix "/storage/sylziyuz/" \
+#     --data_prefix "/storage/$whoami/new_filtered_ann_datasets/" \
+#     --gt_data_prefix "/storage/$whoami/" \
 #     --datasets yandextti-200-mips-10m-subset \
 #     --metrics mips \
 #     --sizes 5000000\
